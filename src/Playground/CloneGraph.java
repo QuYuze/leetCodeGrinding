72c5ab73ba7c923f40d417853fb7e8b6f5b4e339
Qu Yuze <quyuze09@outlook.com>
2024-04-14 18:04:28 +1000

    04/14/2024

---
+++ src/Playground/CloneGraph.java
@@ -0,0 +1,61 @@
+package Playground;
+
+import java.util.HashMap;
+
+//https://leetcode.com/problems/clone-graph/
+public class CloneGraph {
+
+    /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+    class Solution {
+        public Node cloneGraph(Node node) {
+
+            HashMap<Node, Node> map = new HashMap<>();
+            Node clone = cloneNode(node, map);
+
+            return clone;
+
+        }
+
+        private Node cloneNode(Node node, HashMap<Node, Node> map){
+            if(node == null){
+                return null;
+            }
+
+            if(!map.containsKey(node)){
+                Node clone = new Node(node.val);
+                map.put(node, clone);
+                for(Node child : node.neighbors){
+                    if(map.containsKey(child)){
+                        clone.neighbors.add(map.get(child));
+                    }else{
+                        clone.neighbors.add(cloneNode(child, map));
+                    }
+                }
+
+                return clone;
+            }else{
+                return map.get(node);
+            }
+
+        }
+    }
+}
